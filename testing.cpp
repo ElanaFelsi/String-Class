7db927b56433ebcf4ac32e704c011bc55620f266
ElanaFelsi <elanaehrman@gmail.com>
2019-09-19 20:27:43 +0300

    fixed

--- testing.cpp
+++ testing.cpp
@@ -5,14 +5,13 @@ void test()
 {
     MyString myString1("Hello");
     MyString myString2("World");
-    MyString myString3;
+    MyString myString3("");
 
     myString3 = myString1 + myString2;
+    std::cout << myString3;
 
     if (myString1 > myString2)
         std::cout << "Hello is bigger\n";
     else
         std::cout << "World is bigger\n";
-
-    std::cout << myString3;
 }
